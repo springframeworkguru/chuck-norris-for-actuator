274bdea27552708cbdad70b6c7455abec57380fb
Omer Kaya <f.omerkaya@outlook.com>
2020-06-18 20:05:27 -0400

    Changed List and Test

--- chucknorrisforactuator/src/test/java/guru/springframework/norris/chuck/ChuckNorrisQuotesTest.java
+++ chucknorrisforactuator/src/test/java/guru/springframework/norris/chuck/ChuckNorrisQuotesTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.norris.chuck;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
@@ -8,12 +9,15 @@ import static org.junit.Assert.assertNotNull;
  * Created by jt on 12/12/16.
  */
 public class ChuckNorrisQuotesTest {
+    ChuckNorrisQuotes quotes;
+    
+    @Before
+    public void initialize(){
+        quotes = new ChuckNorrisQuotes();
+    }
 
     @Test
     public void testGetRandomQuote() throws Exception {
-
-        ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
-
         assertNotNull(quotes.getRandomQuote());
     }
 }
